8ec6537d02b2496c40ba95bc47cbfee3f185e1d7
YOmling <56666873+YOmling@users.noreply.github.com>
2021-10-21 18:40:58 +0600

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".menu__list");
-const links = document.querySelectorAll(".menu__list li");
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-});
